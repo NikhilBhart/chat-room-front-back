87977b5c1c1f7dc37a2f6c59cf2988d5428a936c
NikhilBhart <inikhilkashyap17@gmail.com>
2025-05-22 11:44:48 +0530

    initial commit

---
+++ chat-app-backend/chat-app-backend/src/main/java/com/substring/chat/payload/MessageRequest.java
@@ -0,0 +1,40 @@
+package com.substring.chat.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageRequest {
+
+    private String content;
+    private String sender;
+    private String roomId;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
